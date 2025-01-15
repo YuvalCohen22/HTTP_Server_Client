d9820fb598f165e9c869ece75d02f5cd6e95de0d
N1ghtmar322 <yuvalcohen1701@gmail.com>
2025-01-15 15:40:46 +0200

    do work, dispatch

--- threadpool.c
+++ threadpool.c
@@ -22,21 +22,98 @@ threadpool* create_threadpool(int num_threads_in_pool, int max_queue_size) {
         return nullptr;
     }
     pThreadpoolSt->qhead = pThreadpoolSt->qtail = nullptr;
-    pthread_mutex_init(&pThreadpoolSt->qlock, nullptr);
-    pthread_cond_init(&pThreadpoolSt->q_not_empty, nullptr);
-    pthread_cond_init(&pThreadpoolSt->q_not_full, nullptr);
+    if (pthread_mutex_init(&pThreadpoolSt->qlock, nullptr) != 0) {
+        perror("init mutex");
+        // free
+        return nullptr;
+    }
+    if (pthread_cond_init(&pThreadpoolSt->q_not_empty, nullptr) != 0) {
+        perror("init cond");
+        //free
+        return nullptr;
+    }
+    if (pthread_cond_init(&pThreadpoolSt->q_not_full, nullptr) != 0) {
+        perror("init cond");
+        //free
+        return nullptr;
+    }
     pThreadpoolSt->shutdown = pThreadpoolSt->dont_accept = 0;
     for (int i = 0; i < num_threads_in_pool; ++i) {
-        if (pthread_create(&pThreadpoolSt->threads[i], nullptr, do_work, pThreadpoolSt) == 0) {
+        if (pthread_create(&pThreadpoolSt->threads[i], nullptr, do_work, pThreadpoolSt) != 0) {
             perror("create thread");
             //free
             return nullptr;
         }
     }
+    return pThreadpoolSt;
 }
+
+void dispatch(threadpool* from_me, dispatch_fn dispatch_to_here, void *arg) {
+    work_t *work = (work_t *) malloc(sizeof(work_t));
+    if (work == NULL) {
+        perror("malloc");
+        return;
+    }
+    work->routine = dispatch_to_here;
+    work->arg = arg;
+    pthread_mutex_lock(&from_me->qlock);
+    if (from_me->dont_accept) {
+        free(work);
+        return;
+    }
+    while (from_me->qsize == from_me->max_qsize) {
+        pthread_cond_wait(&from_me->q_not_full, &from_me->qlock);
+    }
+    if (from_me->dont_accept) {
+        free(work);
+        return;
+    }
+    if (from_me->qsize == 0) {
+        from_me->qhead = from_me->qtail = work;
+    }
+    else {
+        from_me->qtail->next = work;
+        from_me->qtail = from_me->qtail->next;
+    }
+    from_me->qsize++;
+    pthread_cond_signal(&from_me->q_not_empty);
+    pthread_mutex_unlock(&from_me->qlock);
+}
+
 
 void* do_work(void* p) {
     threadpool* thread_pool = (threadpool*) p;
+    while (1) {
+        pthread_mutex_lock(&thread_pool->qlock);
+        if (thread_pool->shutdown) {
+            break;
+        }
+        while (thread_pool->qsize == 0) {
+            pthread_cond_wait(&thread_pool->q_not_empty, &thread_pool->qlock);
+        }
+        if (thread_pool->shutdown) {
+            break;
+        }
+        else {
+            pthread_cond_signal(&thread_pool->q_not_full);
+        }
+        if (thread_pool->qsize == thread_pool->max_qsize) {
+            pthread_cond_signal(&thread_pool->q_not_empty);
+        }
+        work_t* work = thread_pool->qhead;
+        if (thread_pool->qsize) {
+            thread_pool->qtail = nullptr;
+        }
+        thread_pool->qhead = thread_pool->qhead->next;
+        thread_pool->qsize--;
+        if (thread_pool->qsize == 0 && thread_pool->dont_accept) {
+            pthread_cond_signal(&thread_pool->q_empty);
+        }
+        pthread_mutex_unlock(&thread_pool->qlock);
+        work->routine(work->arg);
+        free(work);
+    }
+    pthread_exit(NULL);
 }
 
 
